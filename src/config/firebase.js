a8a7ab415be2bcc835521aa5294a9ed7db0b492d
MindruAndrei <amindru99@gmail.com>
2020-10-24 12:33:52 +0300

    added firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAEmSxaF_3rqmm7UvfxxJjaU14svhrUzuQ",
+    authDomain: "andrew-shop.firebaseapp.com",
+    databaseURL: "https://andrew-shop.firebaseio.com",
+    projectId: "andrew-shop",
+    storageBucket: "andrew-shop.appspot.com",
+    messagingSenderId: "699380832167",
+    appId: "1:699380832167:web:3d4ab8b3805b13a0ed4882"
+  };
+
+  export default firebaseConfig;
